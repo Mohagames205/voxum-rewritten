f6b662f495cea676608073998ecea7baceb68a3b
Mohamed El Yousfi <mohamedelyousfi87@gmail.com>
2022-12-25 18:58:05 +0100

    Fix #4

--- config/database.php
+++ config/database.php
@@ -126,7 +126,6 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             //'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-            'read_write_timeout' => -1
         ],
 
         'default' => [
@@ -136,6 +135,7 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'read_write_timeout' => -1
         ],
 
         'cache' => [
